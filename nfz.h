d991bb2b8f39368f8ca4c93ba217d59077a347bb
Martin Raum <martin@raum-brothers.eu>
2013-12-05 16:31:42 +0100

    Move definition for nfz_ctx to correct header.

--- nfz.h
+++ nfz.h
@@ -1,4 +1,4 @@
-/* nf.h ---  */
+/* nfz.h ---  */
 
 /* Copyright (C) 2013 Martin Raum */
 
@@ -17,13 +17,11 @@
 /* You should have received a copy of the GNU General Public License */
 /* along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
-#ifndef NFZ_MAT_H
-#define NFZ_MAT_H
+#ifndef NFZ_H
+#define NFZ_H
 
 #include "ulong_extras.h"
 #include "flint.h"
-#include "fmpq_mat.h"
-#include "nfz.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -31,29 +29,52 @@ extern "C" {
 
 typedef struct
 {
-    mp_limb_t * entries;
-    slong r;
-    slong c;
-    mp_limb_t ** rows;
-    mp_limb_t *** poly_coefficients;
-    fmpz_poly_t mod;
-}
-nfz_mat_struct;
+  fmpz_poly_t modulus;
 
-typedef nfz_mat_struct nfz_mat_t[1];
+  fmpz_mat_t ev_mat;  
+  fmpz_mat_t int_mat;
+  fmpz_t int_den;
+
+  char * var;
+}
+nfz_ctx_struct;
+
+typedef nfz_ctx_struct nfz_ctx_t[1];
 
 /* Memory managment  *********************************************************/
 
-#define nfz_mat_entry(mat,e,i,j) ((mat)->poly_coefficients[(e)][(i)][(j)])
-#define nfz_mat_nrows(mat) ((mat)->r)
-#define nfz_mat_ncols(mat) ((mat)->c)
+void nfz_ctx_init(nfz_ctx_t ctx, const fmpz_poly_t, const char *var);
+void nfz_ctx_clear(nfz_ctx_t ctx);
 
-void nfz_mat_init(fmpz_mat_t mat, slong rows, slong cols, const nfz_ctx_t ctx);
-void nfz_mat_init_set(fmpz_mat_t mat, const fmpz_mat_t src, const nfz_ctx_t ctx);
-void nfz_mat_set(nfz_mat_t mat1, const nfz_mat_t mat2, const nfz_ctx_t ctx);
-void nfz_mat_clear(nfz_mat_t mat, const nfz_ctx_t ctx);
+statix __inline__
+slong nfz_ctx_degree(const nfz_ctx_t ctx)
+{
+  return fmpz_poly_degree(ctx->modulus);
+}
 
-slong _nfz_mat_rref_mod_prime_generator(nfz_mat_t B, fmpz_t den, const nfz_mat_t A, const nfz_ctx_t ctx, int (*next_prime)(int));
+static __inline__
+int nfz_ctx_fprint(FILE * file, const nfz_ctx_t ctx)
+{
+    int r;
+
+    r = flint_fprintf(file, "modulus =");
+    if (r <= 0)
+	return r;
+
+    r = fmpz_mod_poly_fprint_pretty(file, ctx->modulus, ctx->var);
+    if (r <= 0)
+	return r;
+
+    r = flint_fprintf(file, "\n");
+
+    return r;
+}
+
+static __inline__
+void nfz_ctx_print(const nfz_ctx_t ctx)
+{
+    nfz_ctx_fprint(stdout, ctx);
+}
 
 #ifdef __cplusplus
 }
