5e4e5edf63cc19eba261163ff9fa70133956f47c
Martin Raum <martin@raum-brothers.eu>
2014-02-04 13:39:18 +0100

    Provide full header files for nfz_mat and nfz_mat.

--- nfq_mat.h
+++ nfq_mat.h
@@ -29,7 +29,7 @@
 #include "ulong_extras.h"
 #include "flint.h"
 #include "nfq.h"
-#include "rank_profile.h"
+#include "nfz_mat.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -44,6 +44,136 @@ typedef struct
 } nfq_mat_struct;
 
 typedef nfq_mat_struct nfq_mat_t[1];
+
+#define nfq_mat_entry(mat,i,j) ((mat)->rows[(i)] + (j))
+#define nfq_mat_entry_num(mat,i,j) ((nfz *)(&((*nfq_mat_entry(mat,i,j)).num)))
+#define nfq_mat_entry_den(mat,i,j) ((nfz *)(&((*nfq_mat_entry(mat,i,j)).den)))
+
+#define nfq_mat_nrows(mat) ((mat)->r)
+#define nfq_mat_ncols(mat) ((mat)->c)
+
+void nfq_mat_init(nfq_mat_t mat, slong rows, slong cols, const nfz_ctx_t ctx);
+
+void nfq_mat_clear(nfq_mat_t mat, const nfz_ctx_t ctx);
+
+
+// void nfq_mat_print(const nfq_mat_t mat, const nfz_ctx_t ctx);
+
+/* Basic assignment **********************************************************/
+
+void nfq_mat_set(nfq_mat_t dest, const nfq_mat_t src, const nfz_ctx_t ctx);
+
+void nfq_mat_zero(nfq_mat_t mat, const nfz_ctx_t ctx);
+
+void nfq_mat_one(nfq_mat_t mat, const nfz_ctx_t ctx);
+
+void nfq_mat_transpose(nfq_mat_t rop, const nfq_mat_t op, const nfz_ctx_t ctx);
+
+/* Addition, scalar multiplication  ******************************************/
+
+void nfq_mat_add(nfq_mat_t mat, const nfq_mat_t mat1, const nfq_mat_t mat2,
+		 const nfz_ctx_t ctx);
+
+void nfq_mat_sub(nfq_mat_t mat, const nfq_mat_t mat1, const nfq_mat_t mat2,
+		 const nfz_ctx_t ctx);
+
+void nfq_mat_neg(nfq_mat_t rop, const nfq_mat_t op, const nfz_ctx_t ctx);
+
+void nfq_mat_scalar_mul_nfz(nfq_mat_t rop, const nfq_mat_t op, const nfz_t x,
+			    const nfz_ctx_t ctx);
+
+void nfq_mat_scalar_div_nfz(nfq_mat_t rop, const nfq_mat_t op, const nfz_t x,
+			    const nfz_ctx_t ctx);
+
+/* Basic comparison and properties *******************************************/
+
+int nfq_mat_equal(const nfq_mat_t mat1, const nfq_mat_t mat2,
+		  const nfz_ctx_t ctx);
+
+// int nfq_mat_is_integral(const nfq_mat_t mat, const nfz_ctx_t ctx);
+
+int nfq_mat_is_zero(const nfq_mat_t mat, const nfz_ctx_t ctx);
+
+static __inline__ int
+nfq_mat_is_empty(const nfq_mat_t mat, const nfz_ctx_t ctx)
+{
+    return (mat->r == 0) || (mat->c == 0);
+}
+
+static __inline__ int
+nfq_mat_is_square(const nfq_mat_t mat, const nfz_ctx_t ctx)
+{
+    return (mat->r == mat->c);
+}
+
+/* Integer matrix conversion *************************************************/
+
+int nfq_mat_get_nfz_mat(nfz_mat_t dest, const nfq_mat_t mat, const nfz_ctx_t ctx);
+
+void nfq_mat_get_nfz_mat_entrywise(nfz_mat_t num, nfz_mat_t den,
+    const nfq_mat_t mat, const nfz_ctx_t ctx);
+
+void nfq_mat_get_nfz_mat_matwise(nfz_mat_t num, nfz_t den,
+    const nfq_mat_t mat, const nfz_ctx_t ctx);
+
+void nfq_mat_get_nfz_mat_rowwise(nfz_mat_t num, nfz * den,
+    const nfq_mat_t mat, const nfz_ctx_t ctx);
+
+void nfq_mat_get_nfz_mat_colwise(nfz_mat_t num, nfz * den,
+    const nfq_mat_t mat, const nfz_ctx_t ctx);
+
+void nfq_mat_get_nfz_mat_rowwise_2(nfz_mat_t num, nfz_mat_t num2,
+        nfz * den, const nfq_mat_t mat, const nfq_mat_t mat2, const nfz_ctx_t ctx);
+
+void nfq_mat_get_nfz_mat_mod_nfz(nfz_mat_t dest, const nfq_mat_t mat,
+    const nfz_t mod, const nfz_ctx_t ctx);
+
+void nfq_mat_set_nfz_mat(nfq_mat_t dest, const nfz_mat_t src, const nfz_ctx_t ctx);
+
+void nfq_mat_set_nfz_mat_div_nfz(nfq_mat_t X, const nfz_mat_t Xmod,
+    const nfz_t div, const nfz_ctx_t ctx);
+
+/* Matrix multiplication *****************************************************/
+
+void nfq_mat_mul(nfq_mat_t C, const nfq_mat_t A, const nfq_mat_t B,
+		 const nfz_ctx_t ctx);
+
+void nfq_mat_mul_nfz_mat(nfq_mat_t C, const nfq_mat_t A,
+    const nfz_mat_t B, const nfz_ctx_t ctx);
+
+void nfq_mat_mul_r_nfz_mat(nfq_mat_t C, const nfz_mat_t A,
+    const nfq_mat_t B, const nfz_ctx_t ctx);
+
+/* Trace *********************************************************************/
+
+void nfq_mat_trace(nfq_t trace, const nfq_mat_t mat, const nfz_ctx_t ctx);
+
+/* Determinant ***************************************************************/
+
+void nfq_mat_det(nfq_t det, const nfq_mat_t mat, const nfz_ctx_t ctx);
+
+/* Nonsingular solving *******************************************************/
+
+// int nfq_mat_solve_fraction_free(nfq_mat_t X, const nfq_mat_t A,
+//    const nfq_mat_t B, const nfz_ctx_t ctx);
+
+// int nfq_mat_solve_dixon(nfq_mat_t X, const nfq_mat_t A, const nfq_mat_t B,
+//			const nfz_ctx_t ctx);
+
+/* Inverse *******************************************************************/
+
+int nfq_mat_inv(nfq_mat_t B, const nfq_mat_t A, const nfz_ctx_t ctx);
+
+/* Echelon form **************************************************************/
+
+// int nfq_mat_pivot(slong * perm, nfq_mat_t mat, slong r, slong c, const nfz_ctx_t ctx);
+
+// slong nfq_mat_rref_classical(nfq_mat_t B, const nfq_mat_t A, const nfz_ctx_t ctx);
+
+slong nfq_mat_rref_fraction_free(nfq_mat_t B, const nfq_mat_t A, const nfz_ctx_t ctx);
+
+slong nfq_mat_rref(nfq_mat_t B, const nfq_mat_t A, const nfz_ctx_t ctx);
+
 
 #ifdef __cplusplus
 }
--- nfz_mat.h
+++ nfz_mat.h
@@ -39,11 +39,11 @@ extern "C" {
 
 typedef struct
 {
-    fmpz * entries;
+    nfz * entries;
     slong r;
     slong c;
-    fmpz ** rows;
-    fmpz *** poly_coeffs;
+    nfz ** rows;
+    nfz *** poly_coeffs;
 }
 nfz_mat_struct;
 
@@ -57,27 +57,123 @@ typedef nfz_mat_struct nfz_mat_t[1];
 
 void nfz_mat_init(nfz_mat_t mat, slong rows, slong cols, const nfz_ctx_t ctx);
 void nfz_mat_init_set(nfz_mat_t mat, const nfz_mat_t src, const nfz_ctx_t ctx);
+void nfz_mat_swap(nfz_mat_t mat1, nfz_mat_t mat2, const nfz_ctx_t ctx);
+void nfz_mat_set(nfz_mat_t mat1, const nfz_mat_t mat2, const nfz_ctx_t ctx);
 void nfz_mat_clear(nfz_mat_t mat, const nfz_ctx_t ctx);
 
+int nfz_mat_equal(const nfz_mat_t mat1, const nfz_mat_t mat2, const nfz_ctx_t ctx);
+int nfz_mat_is_zero(const nfz_mat_t mat, const nfz_ctx_t ctx);
 
-void nfz_mat_set(nfz_mat_t mat1, const nfz_mat_t mat2, const nfz_ctx_t ctx);
+static __inline__ int
+nfz_mat_is_empty(const nfz_mat_t mat, const nfz_ctx_t ctx)
+{
+    return (mat->r == 0) || (mat->c == 0);
+}
+
+static __inline__ int
+nfz_mat_is_square(const nfz_mat_t mat, const nfz_ctx_t ctx)
+{
+    return (mat->r == mat->c);
+}
+
+void nfz_mat_zero(nfz_mat_t mat, const nfz_ctx_t ctx);
+void nfz_mat_one(nfz_mat_t mat, const nfz_ctx_t ctx);
 
 
-slong nfz_mat_rref_mod(nfz_mat_t B, fmpz_t den, const nfz_mat_t A, const nfz_ctx_t ctx);
+// int nfz_mat_fprint(FILE * file, const nfz_mat_t mat);
 
-slong _nfz_mat_rref_mod_prime_generator(nfz_mat_t B, fmpz_t den, const nfz_mat_t A, const nfz_ctx_t ctx, int (*next_prime)(const mp_limb_t));
+
+void nfz_mat_transpose(nfz_mat_t B, const nfz_mat_t A, const nfz_ctx_t ctx);
+
+
+/* Addition and subtraction */
+
+void nfz_mat_add(nfz_mat_t C, const nfz_mat_t A, const nfz_mat_t B,
+		 const nfz_ctx_t ctx);
+void nfz_mat_sub(nfz_mat_t C, const nfz_mat_t A, const nfz_mat_t B, const nfz_ctx_t ctx);
+void nfz_mat_neg(nfz_mat_t B, const nfz_mat_t A, const nfz_ctx_t ctx);
+
+/* Scalar operations */
+void nfz_mat_scalar_mul_nfz(nfz_mat_t B, const nfz_mat_t A, const nfz_t c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_mul_fmpz(nfz_mat_t B, const nfz_mat_t A, const fmpz_t c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_mul_si(nfz_mat_t B, const nfz_mat_t A, slong c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_mul_ui(nfz_mat_t B, const nfz_mat_t A, ulong c, const nfz_ctx_t ctx);
+
+void nfz_mat_scalar_addmul_nfz(nfz_mat_t B, const nfz_mat_t A, const nfz_t c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_addmul_fmpz(nfz_mat_t B, const nfz_mat_t A, const fmpz_t c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_addmul_si(nfz_mat_t B, const nfz_mat_t A, slong c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_addmul_ui(nfz_mat_t B, const nfz_mat_t A, ulong c, const nfz_ctx_t ctx);
+
+void nfz_mat_scalar_submul_nfz(nfz_mat_t B, const nfz_mat_t A, const nfz_t c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_submul_fmpz(nfz_mat_t B, const nfz_mat_t A, const fmpz_t c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_submul_si(nfz_mat_t B, const nfz_mat_t A, slong c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_submul_ui(nfz_mat_t B, const nfz_mat_t A, ulong c, const nfz_ctx_t ctx);
+
+void nfz_mat_scalar_divexact_fmpz(nfz_mat_t B, const nfz_mat_t A, const fmpz_t c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_divexact_si(nfz_mat_t B, const nfz_mat_t A, slong c, const nfz_ctx_t ctx);
+void nfz_mat_scalar_divexact_ui(nfz_mat_t B, const nfz_mat_t A, ulong c, const nfz_ctx_t ctx);
+
+/* Multiplication */
+
+void nfz_mat_mul(nfz_mat_t C, const nfz_mat_t A, const nfz_mat_t B, const nfz_ctx_t ctx);
+
+void nfz_mat_sqr(nfz_mat_t B, const nfz_mat_t A, const nfz_ctx_t ctx);
+
+void nfz_mat_pow(nfz_mat_t B, const nfz_mat_t A, ulong exp, const nfz_ctx_t ctx);
+
+
+/* Modular gaussian elimination *********************************************/
+
+slong nfz_mat_rref_mod(nfz_mat_t B, nfz_t den, const nfz_mat_t A, const nfz_ctx_t ctx);
+
+slong _nfz_mat_rref_mod_prime_generator(nfz_mat_t B, nfz_t den, const nfz_mat_t A, const nfz_ctx_t ctx, int (*next_prime)(const mp_limb_t));
+
+slong _nfz_mat_first_non_zero_entry(nfz_t * entry, const nfz_mat_t A, slong r, slong c, const nfz_ctx_t ctx);
+
+void nfz_mat_coeff_bound(nfz_t bound, const nfz_mat_t A, const nfz_ctx_t ctx);
+
+
+/* Trace ********************************************************************/
+
+void nfz_mat_trace(nfz_t trace, const nfz_mat_t mat, const nfz_ctx_t ctx);
+
+/* Determinant **************************************************************/
+
+void nfz_mat_det(nfz_t det, const nfz_mat_t A, const nfz_ctx_t ctx);
+
+/* Characteristic polynomial ************************************************/
+
+void nfz_mat_charpoly(nfz_poly_t cp, const nfz_mat_t mat, const nfz_ctx_t ctx);
+
+/* Rank *********************************************************************/
+
+slong nfz_mat_rank(const nfz_mat_t A, const nfz_ctx_t ctx);
 
 slong nfz_mat_rank_profile(rank_profile_t rk_prof, const nfz_mat_t A, const nfz_ctx_t ctx);
 
-slong _nfz_mat_first_non_zero_entry(fmpz_t * entry, const nfz_mat_t A, slong r, slong c, const nfz_ctx_t ctx);
+/* Nonsingular solving ******************************************************/
 
+int nfz_mat_solve(nfz_mat_t X, nfz_t den,
+        const nfz_mat_t A, const nfz_mat_t B, const nfz_ctx_t ctx);
+
+int nfz_mat_solve_fflu(nfz_mat_t X, nfz_t den,
+        const nfz_mat_t A, const nfz_mat_t B, const nfz_ctx_t ctx);
+
+/* Nullspace ****************************************************************/
+
+slong nfz_mat_nullspace(nfz_mat_t res, const nfz_mat_t mat, const nfz_ctx_t ctx);
+
+/* Inverse ******************************************************************/
+
+int nfz_mat_inv(nfz_mat_t B, nfz_t den, const nfz_mat_t A, const nfz_ctx_t ctx);
+
+/* Modular reduction and reconstruction *************************************/
+
+void nfz_mat_set_nmod_mat(nfz_mat_t A, const nmod_mat_t Amod, const nfz_ctx_t ctx);
 
 void nfz_mat_get_nmod_mat(nf_nmod_mat_t B, const nfz_mat_t A, const nf_nmod_ctx_t ctx_nmod, const nfz_ctx_t ctx);
 
-void nfz_mat_CRT_nmod(nfz_mat_t out, const nfz_mat_t in1, const fmpz_t m1, const nf_nmod_mat_t in2, int sign, const nf_nmod_ctx_t ctx_nmod, const nfz_ctx_t ctx);
-
-
-void nfz_mat_coeff_bound(fmpz_t bound, const nfz_mat_t A, const nfz_ctx_t ctx);
+void nfz_mat_CRT_nmod(nfz_mat_t out, const nfz_mat_t in1, const nfz_t m1, const nf_nmod_mat_t in2, int sign, const nf_nmod_ctx_t ctx_nmod, const nfz_ctx_t ctx);
 
 #ifdef __cplusplus
 }
