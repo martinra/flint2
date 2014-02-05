2fa691b8aa159a9727e573fa1339ebab5924a830
Martin Raum <martin@raum-brothers.eu>
2014-02-05 18:36:49 +0100

    Implement nfz_mat_scalar_mul_fmpz/si/ui.

---
+++ nfz_mat/scalar_mul_si.c
@@ -0,0 +1,40 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2014 Martin Raum
+ 
+******************************************************************************/
+
+#include "fmpz_vec.h"
+
+#include "nfz_mat.h"
+
+
+void
+nfz_mat_scalar_mul_si(nfz_mat_t B, const nfz_mat_t A, slong c,
+		      const nfz_ctx_t ctx)
+{
+  long i, n;
+
+  for (n = 0; n < ctx->deg; ++n)
+    for (i = 0; i < A->r; ++i)
+      _fmpz_vec_scalar_mul_si(B->poly_coeffs[n][i], A->poly_coeffs[n][i], A->c, c);
+}
